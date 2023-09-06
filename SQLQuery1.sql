0b58a361f39c208471a26349806aa57159c28c39
ElberthMartin <1438878@senati.pe>
2023-09-06 16:05:11 -0500

    creamos un procedimiento almacenado llamado sp_buscar_cliente en Sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,5 +10,19 @@ nombre varchar(50),
 apellido varchar(50),
 edad int,
 telefono int
-)
+);
+go
+create proc sp_listar_clientes
+as
+select*from clientes order by codigo
+go
+
+
+create proc sp_buscar_clientes
+@nombre varchar(50)
+as
+select codigo,nombre,edad,telefono from clientes where nombre like @nombre + '%'
+go
+
+
 
