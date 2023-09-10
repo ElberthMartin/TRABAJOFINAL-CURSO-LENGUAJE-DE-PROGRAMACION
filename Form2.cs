085bb80fe61c1e4d4998815c047be94449f175e6
ElberthMartin <1438878@senati.pe>
2023-09-09 22:32:02 -0500

    se agrego la navegacion de botones

--- Form2.cs
+++ Form2.cs
@@ -17,19 +17,31 @@ namespace trabajofinal
             InitializeComponent();
         }
         //INICIAL FORM3
+        // En Form2
+
         private void button1_Click(object sender, EventArgs e)
         {
-
+            // Abrir Form3
+            Form3 form3 = new Form3();
+            form3.Show();
+            this.Hide(); // Opcionalmente, puedes ocultar Form2 si deseas cerrarlo al abrir Form3
         }
-        //PRIMARIA FORM4
+
         private void button3_Click(object sender, EventArgs e)
         {
-
+            // Abrir Form4
+            Form4 form4 = new Form4();
+            form4.Show();
+            this.Hide(); // Opcionalmente, puedes ocultar Form2 si deseas cerrarlo al abrir Form4
         }
-        //SECUNDARIA FORM5
+
         private void button2_Click(object sender, EventArgs e)
         {
-           
+            // Abrir Form5
+            Form5 form5 = new Form5();
+            form5.Show();
+            this.Hide(); // Opcionalmente, puedes ocultar Form2 si deseas cerrarlo al abrir Form5
         }
+
     }
 }
