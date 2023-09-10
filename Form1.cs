01e924a98d733d279e72c2878a2dc2c874e97029
ElberthMartin <1438878@senati.pe>
2023-09-09 20:34:08 -0500

    see specifico con comentarios

--- Form1.cs
+++ Form1.cs
@@ -25,24 +25,6 @@ namespace trabajofinal
             // Lógica a ser executada quando o texto do textBox1 é alterado.
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string usuario = textBox1.Text;
-            string contraseña = textBox2.Text;
-
-            // Verificar si el usuario y la contraseña son "admin"
-            if (usuario == "admin" && contraseña == "admin")
-            {
-                // Si es correcto, abrir Form2 y cerrar este formulario
-                Form2 form2 = new Form2();
-                form2.Show();
-                this.Hide();
-            }
-            else
-            {
-                // Mostrar un mensaje de error
-                MessageBox.Show("Usuario o contraseña incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+       //Aquí se agregaran usuarios
     }
 }
